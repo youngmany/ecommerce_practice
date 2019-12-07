1b171ace9d9bbf7465c7ec2ae4a0801558c59344
youngmany <ppp4397@gmail.com>
2019-12-07 14:17:42 +0900

    Mod: TestCase setUp 적용

--- account/tests.py
+++ account/tests.py
@@ -4,22 +4,25 @@ from django.contrib.auth.models import User
 
 class UserModelTest(TestCase):
 
+    def setUp(self):
+        self.first_user = User(
+            username='testuser1',
+            password='123123'
+        )
+        self.first_user.save()
+
+        self.second_user = User(
+            username='testuser2',
+            password='123123'
+        )
+        self.second_user.save()
+
     def test_create_user(self):
-        first_user = User()
-        first_user.username = 'testuser1'
-        first_user.password = '123123'
-        first_user.save()
-
-        second_user = User()
-        second_user.username = 'testuser2'
-        second_user.password = '123123'
-        second_user.save()
-
         saved_users = User.objects.all()
         self.assertEqual(saved_users.count(), 2)
 
         first_saved_item = saved_users[0]
         second_saved_item = saved_users[1]
 
-        self.assertEqual(first_user.username, 'testuser1')
-        self.assertEqual(second_user.username, 'testuser2')
+        self.assertEqual(self.first_user.username, 'testuser1')
+        self.assertEqual(self.second_user.username, 'testuser2')
