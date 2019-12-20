5762db9a4c3a88371cb63d5e23e2b9954f2a6421
youngmany <ppp4397@gmail.com>
2019-12-20 19:58:01 +0900

    Mod: pytest 환경 구성 수정

--- account/tests.py
+++ account/tests.py
@@ -1,43 +1,47 @@
-from django.test import TestCase
+import pytest
+from django.shortcuts import reverse
 from django.contrib.auth.models import User
 from .forms import RegisterForm, LoginForm
 
 
-class UserModelTest(TestCase):
+@pytest.mark.django_db
+def test_user_create():
+    obj = User.objects.create(
+        username='ymkim1',
+        password=123123
+    )
+    assert obj
 
-    def setUp(self):
-        self.first_user = User(
-            username='testuser1',
-            email='test1@test.com',
-            password='123123'
-        )
-        self.first_user.save()
-
-        self.second_user = User(
-            username='testuser2',
-            email='test2@test.com',
-            password='123123'
-        )
-        self.second_user.save()
-
-        self.t_user = {
-            'email':'t@t123.com',
-            'password':123123,
-            're_password':123123
+def test_user_register_form():
+    form = RegisterForm(
+        data={
+            'username': 'ymkim1',
+            'password': 123123,
+            're_password': 123123,
         }
+    )
 
-    def test_create_user(self):
-        saved_users = User.objects.all()
-        self.assertEqual(saved_users.count(), 2)
+    assert form.is_valid()
 
-        first_saved_item = saved_users[0]
-        second_saved_item = saved_users[1]
 
-        self.assertEqual(self.first_user.email, 'test1@test.com')
-        self.assertEqual(self.second_user.email, 'test2@test.com')
-    
-    def test_user_forms(self):
-        form = RegisterForm(
-            self.t_user
-        )
-        # self.assertTrue(form.is_valid())
+
+"""
+    form = LoginForm(
+        data={
+            'username': 'ymkim1',
+            'password': 123123,
+        }
+    )
+
+    assert form.is_valid()
+
+def test_user_login_form():
+    form = LoginForm(
+        data={
+            'username': 'ymkim1',
+            'password': 123123,
+        }
+    )
+
+    assert form.is_valid()
+"""
